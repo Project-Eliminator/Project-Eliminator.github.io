4fdce668b5c88524262bfc15e441e4eedf3258b7
Project-Eliminator <bimsp136@re146.dev>
2025-08-11 17:40:34 +0300

    Create config.ts

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "Project Eliminator",
+  description: "Documentation for Project Eliminator",
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/installation' },
+      { text: 'Download', link: '/download/' },
+    ],
+    sidebar: {
+      '/': [
+        {
+          text: 'Guide',
+          items: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/installation' },
+      { text: 'Download', link: '/download/' },
+          ]
+        }
+      ],
+    },
+    socialLinks: [
+      { icon: 'telegram', link: 'https://t.me/project_elimiantor' }
+    ]
+  }
+})
